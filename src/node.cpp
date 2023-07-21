950dec477dc17f24e27b7eaa4158df01fe791154
HietanResearch <hietan.research@gmail.com>
2023-07-21 20:53:59 +0900

    [CLEAN] add spaces

--- src/node.cpp
+++ src/node.cpp
@@ -1,19 +1,19 @@
 #include "node.hpp"
 
 namespace node {
-	Node::Node(){};
-	Node::Node(int arg_x, int arg_y): x{arg_x}, y{arg_y} {};
+	Node::Node() {};
+	Node::Node(int arg_x, int arg_y) : x{arg_x}, y{arg_y} {};
 	Node::Node(int arg_x, int arg_y, int arg_id) : x{arg_x}, y{arg_y}, id{arg_id} {};
 
-	float abs(Node n){
+	float abs(Node n) {
 		return std::sqrt(std::pow(n.x, 2) + std::pow(n.y, 2));
 	}
 
-	float distance(Node n1, Node n2){
+	float distance(Node n1, Node n2) {
 		return abs(n1 - n2);
 	}
 
-	Node Node::operator - (Node node){
+	Node Node::operator - (Node node) {
 		return Node(this->x - node.x, this->y - node.y);
 	}
 }
