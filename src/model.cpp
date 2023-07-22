c8f46f76056f7d12172190b16320b11d7993c368
HietanResearch <hietan.research@gmail.com>
2023-07-22 11:52:08 +0900

    [CLEAN] Refactor class "Model"

--- src/model.cpp
+++ src/model.cpp
@@ -1,44 +1,44 @@
 #include "model.hpp"
 
-Model::Model(int arg_n, int arg_map_size) : n{arg_n}, map_size{arg_map_size} {
+Model::Model(int arg_n, int arg_size) : n{arg_n}, size{arg_size} {
 	c = std::vector<std::vector<float>>(n, std::vector<float>(n));
 	generateModel();
 }
 
-int Model::getN(){
+int Model::getN() const {
 	return n;
 }
 
-std::vector<std::vector<float>> Model::getC(){
+std::vector<std::vector<float>> Model::getC() const {
 	return c;
 }
 
 void Model::generateModel() {
 	std::random_device rd;
 	std::mt19937 mt(rd());
-	std::uniform_real_distribution<float> dist(0, map_size);
+	std::uniform_real_distribution<float> dist(0, size);
 
 	nodes = std::vector<node::Node>(n);
 
-	for(int i = 0; i < n; i++){
+	for(int i = 0; i < n; i++) {
 		int x = dist(mt);
 		int y = dist(mt);
 		nodes.at(i) = node::Node(x, y, i);
 	}
 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j <= i; j++){
-			if(i == j) c.at(i).at(j) = FLT_MAX;
+	for(int y = 0; y < n; y++) {
+		for(int x = 0; x <= y; x++) {
+			if(y == x) c.at(y).at(x) = FLT_MAX;
 			else {
-				float d = distance(nodes.at(i), nodes.at(j));
-				c.at(i).at(j) = d;
-				c.at(j).at(i) = d;
+				float d = distance(nodes.at(y), nodes.at(x));
+				c.at(y).at(x) = d;
+				c.at(x).at(y) = d;
 			}
 		}
 	}
 }
 
-std::ofstream& operator << (std::ofstream& out, const Model& g){
+std::ofstream& operator << (std::ofstream& out, const Model& g) {
 	for(const node::Node& n: g.nodes) out << n;
 	return out;
 }
--- src/model.hpp
+++ src/model.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "node.hpp"
-#include <cmath>
 #include <cfloat>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <random>
@@ -11,13 +11,13 @@
 class Model {
 	private:
 		const int n;
-		const int map_size;
+		const int size;
 		std::vector<node::Node> nodes;
 		std::vector<std::vector<float>> c;
 	public:
-		Model(int arg_n, int arg_map_size);
-		int getN();
-		std::vector<std::vector<float>> getC();
+		Model(int arg_n, int arg_size);
+		int getN() const;
+		std::vector<std::vector<float>> getC() const;
 		void generateModel();
 		friend std::ofstream& operator<<(std::ofstream& out, const Model& g);
 };
