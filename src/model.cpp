9702e8fa0eb95fa25ca83abcf548ac2eb0a16894
HietanResearch <hietan.research@gmail.com>
2023-07-22 13:11:04 +0900

    [CLEAN] Remove empty line

--- src/model.cpp
+++ src/model.cpp
@@ -19,9 +19,7 @@ std::vector<std::vector<float>> Model::getC() const {
 void Model::generateRandom() {
 	std::random_device rd;
 	std::mt19937 mt;
-
 	mt.seed(seed ? seed : rd());
-
 	std::uniform_real_distribution<float> dist(0, size);
 
 	nodes = std::vector<node::Node>(n);
